c482319eced1dd3e322a846fbce3b93ff0a508db
Massaran Konate <massaran.konate@sascholar.org>
2019-02-08 14:48:29 +0000

    Massaran is BAWSE

--- buttons.js
+++ buttons.js
@@ -1,5 +1,50 @@
 let popbutton = document.querySelector("#popbutton")
 
 popbutton.addEventListener("click", e => {
-    console.log("pop button clicked!")
+  if(popbutton.classList.contains("pop") == true){
+    popbutton.classList.remove("pop")
+  }else{
+    popbutton.classList.add("pop")
+  } 
 })
+
+let bigbutton = document.querySelector("#bigbutton")
+
+bigbutton.addEventListener("click", e => {
+  if(bigbutton.classList.contains("big") == true){
+    bigbutton.classList.remove("big")
+  }else{
+    bigbutton.classList.add("big")
+  } 
+})
+
+let fancybutton = document.querySelector("#fancybutton")
+
+fancybutton.addEventListener("click", e => {
+  if(fancybutton.classList.contains("fancy") == true){
+    fancybutton.classList.remove("fancy")
+  }else{
+    fancybutton.classList.add("fancy")
+  } 
+})
+
+let negativebutton = document.querySelector("#negativebutton")
+
+negativebutton.addEventListener("click", e => {
+  if(negativebutton.classList.contains("negative") == true){
+    negativebutton.classList.remove("negative")
+  }else{
+    negativebutton.classList.add("negative")
+  } 
+})
+
+let awesomebutton = document.querySelector("#awesomebutton")
+
+awesomebutton.addEventListener("click", e => {
+  if(awesomebutton.classList.contains("awesome") == true){
+    awesomebutton.classList.remove("awesome")
+  }else{
+    awesomebutton.classList.add("awesome")
+  } 
+})
+
